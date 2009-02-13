2953df4c4c6bc31caf5d2483893191478551f8cd
Maxxie <boys.dont.cry@live.it>
2009-02-13 14:07:41 +0100

    [p84] Fix: Restore Judgement of Wisdom effect.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -5400,7 +5400,10 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                 case 20186:
                 {
                     if (pVictim->getPowerType() == POWER_MANA)
-                        pVictim->CastSpell(pVictim, 20268, true, 0, triggeredByAura);
+                    {
+                        uint32 gainMana = pVictim->GetMaxPower(POWER_MANA) * triggeredByAura->GetBasePoints() / 100;
+                        pVictim->CastCustomSpell(pVictim, 20268, &gainMana, 0, 0, true, 0, triggeredByAura);
+                    }
                     return true;
                 }
                 // Holy Power (Redemption Armor set)
